d6d0e5473559093b60fd06ff116ec290ade7fcbc
Reshetov Yuriy <ryv1983@mail.ru>
2024-05-03 22:46:48 +0300

    darray plus qsort and radixmap upload

---
+++ ex35_arraysort/src/libarr_sort/radixmap.c
@@ -0,0 +1,174 @@
+/*
+  * Based on code by Andre Reinald then heavily modified by Zed A. Shaw.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <libarr_sort/radixmap.h>
+#include <lcthw/dbg.h>
+
+RadixMap *RadixMap_create(size_t max)
+{
+    RadixMap *map = calloc(1, sizeof(RadixMap));
+    check_mem(map);
+
+    map->contents = calloc(max, sizeof(RMElement));
+    check_mem(map->contents);
+
+    map->temp = calloc(max, sizeof(RMElement));
+    check_mem(map->temp);
+
+    map->max = max;
+    map->size = 0;
+
+    return map;
+error:
+    if (map)
+    {
+        if (map->contents)
+            free(map->contents);
+        if (map->temp)
+            free(map->temp);
+        free(map);
+    }
+    return NULL;
+}
+
+void RadixMap_destroy(RadixMap *map)
+{
+    if (map)
+    {
+        free(map->contents);
+        free(map->temp);
+        free(map);
+    }
+}
+
+#define ByteOf(x, y) (((uint8_t *)x)[(y)])
+
+static inline void radix_sort(short offset, uint64_t max,
+                              uint64_t *source, uint64_t *dest)
+{
+    uint64_t count[256] = {0};
+    uint64_t *cp = NULL;
+    uint64_t *sp = NULL;
+    uint64_t *size = NULL;
+    uint64_t s = 0;
+    uint64_t c = 0;
+
+    // Ref radixmap.png (LOOP 1/ LOOP 2/ LOOP3)
+
+    // count occurences of every byte value
+    for (sp = source, size = source + max; sp < size; sp++)
+    {
+        count[ByteOf(sp, offset)]++;
+    }
+
+    // transform count into index by summing
+    // elements and storing into same array
+    for (s = 0, cp = count, size = count + 256; cp < size; cp++)
+    {
+        c = *cp;
+        *cp = s;
+        s += c;
+    }
+
+    // fill dest with the right values in the right place
+    for (sp = source, size = source + max; sp < size; sp++)
+    {
+        cp = count + ByteOf(sp, offset);
+        dest[*cp] = *sp;
+        ++(*cp);
+    }
+}
+
+void RadixMap_sort(RadixMap *map)
+{
+    uint64_t *source = &map->contents[0].raw;
+    uint64_t *temp = &map->temp[0].raw;
+
+    radix_sort(0, map->size, source, temp);
+    radix_sort(1, map->size, temp, source);
+    radix_sort(2, map->size, source, temp);
+    radix_sort(3, map->size, temp, source);
+}
+
+RMElement *RadixMap_find(RadixMap *map, uint32_t to_find)
+{
+    int low = 0;
+    int high = map->size - 1;
+    RMElement *data = map->contents;
+
+    while (low <= high)
+    {
+        int middle = low + (high - low) / 2;
+        uint32_t key = data[middle].data.key;
+
+        if (to_find < key)
+        {
+            high = middle - 1;
+        }
+        else if (to_find > key)
+        {
+            low = middle + 1;
+        }
+        else
+        {
+            return &data[middle];
+        }
+    }
+
+    return NULL;
+}
+
+int RadixMap_add(RadixMap *map, uint32_t key, uint32_t value)
+{
+    check(key < UINT32_MAX, "Key can't be equal to UINT32_MAX.");
+
+    RMElement element = {.data = {.key = key, .value = value}};
+    check(map->size < map->max, "RadixMap is full.");
+
+    map->contents[map->size++] = element;
+
+    RadixMap_sort(map);
+
+    return 0;
+
+error:
+    return -1;
+}
+
+int RadixMap_delete(RadixMap *map, RMElement *el)
+{
+    check(map->size > 0, "There is nothing to delete.");
+    check(el != NULL, "Can't delete a NULL element.");
+
+    el->data.key = UINT32_MAX;
+
+    if (map->size > 1)
+    {
+        // don't bother resorting a map of 1 length
+        RadixMap_sort(map);
+    }
+
+    map->size--;
+
+    return 0;
+error:
+    return -1;
+}
+
+int RadixMap_display(RadixMap *map)
+{
+    printf("=============Radimax_info==============\n");
+
+    for (size_t i = 0; i < map->max; i++)
+    {
+        printf("Element: %ld ; Key: %u ; Value : %u \n", i, map->contents[i].data.key, map->contents[i].data.value);
+    }
+
+    printf("=======================================\n");
+
+    return 0;
+}
---
+++ ex35_arraysort/tests/radixmap_tests.c
@@ -0,0 +1,119 @@
+#include "minunit.h"
+#include <libarr_sort/radixmap.h>
+#include <time.h>
+
+static int make_random(RadixMap *map)
+{
+    size_t i = 0;
+
+    for (i = 0; i < map->max; i++)
+    {
+        uint32_t key = (uint32_t)(rand() | (rand() << 16));
+        check(RadixMap_add(map, key, i) == 0, "Failed to add key %u.",
+              key);
+    }
+
+    RadixMap_display(map);
+
+    return i;
+
+error:
+    return 0;
+}
+
+static int check_order(RadixMap *map)
+{
+    RMElement d1, d2;
+    unsigned int i = 0;
+
+    // only signal errors if any (should not be)
+    for (i = 0; map->size > 0 && i < map->size - 1; i++)
+    {
+        d1 = map->contents[i];
+        d2 = map->contents[i + 1];
+
+        if (d1.data.key > d2.data.key)
+        {
+            debug("FAIL:i=%u, key: %u, value: %u, equals max? %d\n", i,
+                  d1.data.key, d1.data.value,
+                  d2.data.key == UINT32_MAX);
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+static int test_search(RadixMap *map)
+{
+    unsigned i = 0;
+    RMElement *d = NULL;
+    RMElement *found = NULL;
+
+    for (i = map->size / 2; i < map->size; i++)
+    {
+        d = &map->contents[i];
+        found = RadixMap_find(map, d->data.key);
+        check(found != NULL, "Didn't find %u at %u.", d->data.key, i);
+        check(found->data.key == d->data.key,
+              "Got the wrong result: %p:%u looking for %u at %u", found,
+              found->data.key, d->data.key, i);
+        debug("Key:%u value: %u", found->data.key, found->data.value);
+    }
+
+    return 1;
+error:
+    return 0;
+}
+
+// test for big number of elements
+static char *
+test_operations()
+{
+    size_t N = 200;
+
+    RadixMap *map = RadixMap_create(N);
+    mu_assert(map != NULL, "Failed to make the map.");
+    mu_assert(make_random(map), "Didn't make a random fake radix map.");
+
+    RadixMap_sort(map);
+    mu_assert(check_order(map),
+              "Failed to properly sort the RadixMap.");
+
+    mu_assert(test_search(map), "Failed the search test.");
+    mu_assert(check_order(map),
+              "RadixMap didn't stay sorted after search.");
+
+    while (map->size > 0)
+    {
+        RMElement *el = RadixMap_find(map,
+                                      map->contents[map->size / 2].data.key);
+        mu_assert(el != NULL, "Should get a result.");
+
+        size_t old_size = map->size;
+
+        mu_assert(RadixMap_delete(map, el) == 0, "Didn't delete it.");
+        mu_assert(old_size - 1 == map->size, "Wrong size after delete.");
+
+        // test that the size is now the old value,
+        // but uint32 max so it trails off
+        mu_assert(check_order(map),
+                  "RadixMap didn't stay sorted after delete.");
+    }
+
+    RadixMap_destroy(map);
+
+    return NULL;
+}
+
+char *all_tests()
+{
+    mu_suite_start();
+    srand(time(NULL));
+
+    mu_run_test(test_operations);
+
+    return NULL;
+}
+
+RUN_TESTS(all_tests);
