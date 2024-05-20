17b613e9930edbf9de0548b1021bf96267d36f1c
Reshetov Yuriy <ryv1983@mail.ru>
2024-05-20 13:41:44 +0300

    Little change to traversing stack

--- ex42_stack_queue/src/lcthw/queue.h
+++ ex42_stack_queue/src/lcthw/queue.h
@@ -2,10 +2,10 @@
 #include <lcthw/dbg.h>
 
 #define Queue List
-#define Queue_create() List_create()
-#define Queue_destroy(queue) List_destroy(queue)
-#define Queue_send(queue, value) List_push(queue, value)
-#define Queue_peek(queue) List_first(queue)
-#define Queue_count(queue) List_count(queue)
+#define Queue_create List_create
+#define Queue_destroy List_destroy
+#define Queue_send List_push
+#define Queue_peek List_first
+#define Queue_count List_count
 #define QUEUE_FOREACH(queue, cur) LIST_FOREACH(queue, first, next, cur)
-#define Queue_recv(queue) List_shift(queue)
+#define Queue_recv List_shift
--- ex42_stack_queue/src/lcthw/stack.h
+++ ex42_stack_queue/src/lcthw/stack.h
@@ -2,10 +2,10 @@
 #include <lcthw/dbg.h>
 
 #define Stack List
-#define Stack_create() List_create()
-#define Stack_destroy(stack) List_destroy(stack)
-#define Stack_push(stack, value) List_push(stack, value)
-#define Stack_peek(stack) List_last(stack)
-#define Stack_count(stack) List_count(stack)
-#define STACK_FOREACH(stack, cur) LIST_FOREACH(stack, first, next, cur)
-#define Stack_pop(stack) List_pop(stack)
+#define Stack_create List_create
+#define Stack_destroy List_destroy
+#define Stack_push List_push
+#define Stack_peek List_last
+#define Stack_count List_count
+#define STACK_FOREACH(stack, cur) LIST_FOREACH(stack, last, prev, cur)
+#define Stack_pop List_pop
